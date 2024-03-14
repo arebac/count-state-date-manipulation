20d9856edbc18c5e3b0a6aa72e34bc4904c56a31
arebac <j.cabreraortiz@outlook.com>
2024-03-14 08:47:17 -0400

    State Practice.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Count from "./assets/components/Count";
+import "./App.css"
+function App() {
+  return (
+    <div className="entirePage">
+      <Count />
+    </div>
+  );
+}
+
+export default App;
